2f31d6eec34f8a8911e304a17670b0ca4b62375b
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-28 16:27:07 +0100

    validations of all models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one :user, through: :loans
+  validates :item_name, :description, :img_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :loans
+  has_many :reviews
+  has_many :items, through: :loans
+  has_many :reviews, through: :loans
+  has_many :messages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :location, presence: true
+
 end
