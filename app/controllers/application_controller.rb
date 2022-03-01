6bad4b0cf2535e44a8e775d97e56769e36d0cfb7
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-03-01 12:35:29 +0100

    User registration working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :location])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :location])
   end
-
 end
