a476c00386782b00179735b9ed5af9e81a1e9484
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-03-01 16:54:36 +0100

    added mapbox functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
   has_one_attached :photo
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
