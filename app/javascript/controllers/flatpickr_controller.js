700b7f13e089a6e7d43f09faaa5536b949ea40f3
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-03-02 17:03:11 +0100

    before pull

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,9 +1,9 @@
-import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr";
+// import { Controller } from "@hotwired/stimulus"
+// import flatpickr from "flatpickr";
 
-export default class extends Controller {
-  connect() {
-    // const fp = flatpickr(".flatpickr-input", {});
-    console.log("hello")
-  }
-}
+// export default class extends Controller {
+//   connect() {
+//     // const fp = flatpickr(".flatpickr-input", {});
+//     console.log("hello")
+//   }
+// }
