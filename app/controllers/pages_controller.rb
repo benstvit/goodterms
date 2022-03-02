81d1a1dc11bb50e37dac4529e194bf2885c3930a
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-03-02 11:29:10 +0100

    loan-cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @users = User.all
+    @borrowed = current_user.loans.each do |loan|
+      loan.where(loan.item.user_id != current_user.id)
+    end
+    @lent = current_user.loans.each do |loan|
+      loan.where(loan.item.user_id == current_user.id)
+    end
   end
 
   def dashboard
