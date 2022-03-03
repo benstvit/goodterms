eece80353fa7286b19302556ba34feb35e616dea
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-03-03 17:34:57 +0100

    added strong params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,8 +8,20 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @loan = Loan.find(params[:loan_id])
+    @review.loan = @loan
+    @review.save
+    redirect_to root_path
   end
 
   def destroy
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
 end
