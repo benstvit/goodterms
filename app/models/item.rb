6692616c517c4d129d8a516ffe0146041c06a08c
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-03-03 10:39:14 +0100

    feature working

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :loans
   validates :item_name, :description, presence: true
+  after_commit :add_default_photo, on: [:create, :update]
   has_one_attached :photo
+
+  private
+
+  def add_default_photo
+    unless photo.attached?
+      self.photo.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-img.png")), filename: 'default-img.png', content_type: "image/png")
+    end
+  end
 end
