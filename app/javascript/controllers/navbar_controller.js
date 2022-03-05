01b32f36a6990608321808ffc672456d308ed332
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-03-05 12:36:31 +0100

    buttons appear in navbar BAM

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,11 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['lend', 'borrow']
+
   updateNavbar() {
     if (window.scrollY >= window.innerHeight) {
       this.element.classList.add("navbar-lewagon-white")
+      this.borrowTarget.style.visibility = "visible"
+      this.lendTarget.style.visibility = "visible"
     } else {
+      this.borrowTarget.style.visibility = "hidden"
+      this.lendTarget.style.visibility = "hidden"
       this.element.classList.remove("navbar-lewagon-white")
+
     }
   };
 }
