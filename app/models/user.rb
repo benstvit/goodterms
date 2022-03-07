01f6b125c206e6e3bed80f2190791e09bdff1ce3
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-03-07 16:19:26 +0100

    Working on reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :items
   has_many :loans
-  has_many :reviews
+  has_many :reviews, through: :loans, dependent: :destroy
   has_many :messages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
