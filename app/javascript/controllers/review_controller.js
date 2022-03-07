41430f7696a53badb9026d915386ec7a8d0d7619
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-03-07 10:15:33 +0100

    styling added

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    console.log('Hey mate')
     let modal = document.getElementById("myModal");
 
 
